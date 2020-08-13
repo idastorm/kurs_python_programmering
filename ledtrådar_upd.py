ca181dbe02e4a56ce9f8173938968de90a959e51
idastorm <31061856+idastorm@users.noreply.github.com>
2020-08-13 13:12:12 +0200

    Add files via upload
    Ett första utkast. "löner.xlsx" och "ledtrådar_upd.py" behövs för att köra "intro.ipynb"

---
+++ ledtrådar_upd.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 21 14:53:25 2020
+
+@author: Ida Storm
+"""
+
+def ledtråd1():
+    print("Förslagsvis skapar du en ny lista och lägger till ett värde varje gång du loopar över en lön")
+    
+def ledtråd2():
+    print("Varje nytt värde ska vara 5% av lönen, lön*0.05 i loopen får fram detta värdet")
+    
+def ledtråd3():
+    print("sum() är en inbyggd funktion som ger det totala värdet av alla tal i en lista")
+    
+def lösning1():
+    print("bonusar=[]\nfor lön in löner:\n   bonus=lön*0.05\n   bonusar.append(bonus)\nprint(sum(bonusar)")
